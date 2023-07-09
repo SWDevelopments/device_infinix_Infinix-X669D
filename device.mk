2ddbecf975cbc0790abbcd072b137c18a156246b
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-07-09 06:34:56 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/infinix/X669D
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -15,14 +9,14 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    bootctrl.ums9230
+
+PRODUCT_SHARED_BOOT_CONTROL_HAL := \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.ums9230
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.ums9230 \
+    $(PRODUCT_SHARED_BOOT_CONTROL_HAL) \
     libgptutils \
     libz \
     libcutils
@@ -33,3 +27,6 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Additional device-specific configurations
+# Add your custom configurations for Infinix Hot 30i X669D Android 12.1 here
